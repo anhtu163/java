f494c610a4caa24043df8b57b3ad309108a944f9
anhtu <glenntu163@gmail.com>
2020-10-20 17:16:58 +0700

    Practice programming Spring Core with I18n, SpringMVC

---
+++ SpringBootI18n/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.config;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	@Bean(name = "localeResolver")
+	public LocaleResolver getLocaleResolver() {
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+		resolver.setCookieDomain("myApplicationCookie");
+		resolver.setDefaultLocale(Locale.ENGLISH);
+		
+		resolver.setCookieMaxAge(60*60);
+		return resolver;
+	}
+	
+	@Bean(name = "messageSource")
+	public MessageSource getMessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		registry.addInterceptor(localeChangeInterceptor).addPathPatterns("/*");
+	}
+}
